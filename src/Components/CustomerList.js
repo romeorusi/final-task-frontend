d77efa5d09e00d907412a95a96fc48e9aa705457
romeorusi <Romeo.rusi@gmail.com>
2022-12-07 15:27:06 +0200

    Final task 1 complete

---
+++ src/Components/CustomerList.js
@@ -0,0 +1,70 @@
+import React, { useState, useEffect } from "react";
+import { AgGridReact } from "ag-grid-react";
+import "ag-grid-community/styles/ag-grid.css";
+import "ag-grid-community/styles/ag-theme-material.css";
+import IconButton from "@mui/material/IconButton";
+import DeleteIcon from "@mui/icons-material/Delete";
+import Button from '@mui/material/Button';
+
+
+
+
+
+
+export default function CustomerList(){
+    const [customers, setCustomers] = useState([]);
+
+    useEffect(()=>{
+        console.log("Ollaaan hook jutussa");
+        fetchData();
+    }, [])
+
+
+    const fetchData = () => {
+        console.log("Customerlistin fetchdata funktiossa")
+        fetch("https://customerrest.herokuapp.com/api/customers")
+        .then(response => response.json())
+        .then(data => setCustomers(data.content));
+    }
+
+    const deleteCustomer = (link) =>{
+        console.log("Customerlistin delete funktiossa")
+        fetch(link, { method: "DELETE" }).then((response) => {
+            if (response.ok) {
+              fetchData();
+            }
+          });
+    }
+
+
+    const [columnDefs, setColumnDefs] = useState([
+        { headerName: "firstname", field: "firstname", sortable: true, filter: true, floatingFilter:true},
+        { headerName: "lastname", field: "lastname", sortable: true, filter: true, floatingFilter:true },
+        { headerName: "streetaddress", field: "streetaddress", sortable: true, filter: true, floatingFilter:true },
+        { headerName: "postcode", field: "postcode", sortable: true, filter: true, floatingFilter:true },
+        { headerName: "city", field: "city", sortable: true, filter: true, floatingFilter:true },
+        { headerName: "email", field: "email", sortable: true, filter: true, floatingFilter:true },
+        { headerName: "phone", field: "phone", sortable: true, filter: true, floatingFilter:true }])
+
+
+    return(
+        <div>
+            <div style={{ height: "100%", boxSizing: "border-box" }}>
+        <div
+          style={{ height: 600, width: "90%" }}
+          className="ag-theme-material"
+        >
+          <AgGridReact
+            rowData={customers}
+            columnDefs={columnDefs}
+            paginationPageSize={10}
+            pagination={true}
+          />
+        </div>
+      </div>
+
+
+
+        </div>
+    )
+}
---
+++ src/Components/TrainingList.js
@@ -0,0 +1,81 @@
+import React, { useState, useEffect } from "react";
+import { AgGridReact } from "ag-grid-react";
+import "ag-grid-community/styles/ag-grid.css";
+import "ag-grid-community/styles/ag-theme-material.css";
+import IconButton from "@mui/material/IconButton";
+import DeleteIcon from "@mui/icons-material/Delete";
+import Button from '@mui/material/Button';
+import dayjs from "dayjs";
+
+
+
+
+export default function TrainingList({CustomerList}){
+    const [trainings, setTrainings] = useState([]);
+    const [tieto, setTieto] = useState('');
+
+
+    useEffect(()=>{
+        console.log("Ollaaan hook jutussa ei tiedetä miks");
+        fetchData();
+    }, [])
+    
+
+    const fetchData = () => {
+        console.log("haetaan treenejä");
+        fetch("https://customerrest.herokuapp.com/gettrainings")
+        .then((response) => response.json())
+        .then((data) => setTrainings(data));
+    }
+
+   
+    const [columnDefs, setColumnDefs] = useState([
+        //{ headerName: "id", field: "brand", sortable: true, filter: true},
+        { 
+            headerName: "date", 
+            field: "date", 
+            sortable: true, 
+            filter: true, 
+            floatingFilter: true,
+            cellRendererFramework: (params) => {
+              const date = dayjs(params.value); // Convert the date value to a dayjs object
+              const formattedDate = date.format('DD.MM.YYYY HH:mm'); // Use the format method to convert the date to the desired format
+              return <div>{formattedDate}</div>; // Return the formatted date as the content of the cell
+            }
+          },
+        { headerName: "duration", field: "duration", sortable: true, filter: true, floatingFilter:true },
+        { headerName: "activity", field: "activity", sortable: true, filter: true, floatingFilter:true },
+        {   headerName: "customer",
+            field: "customer",
+            sortable: true,
+            filter: true,
+            floatingFilter: true,
+            cellRendererFramework: (params) => (
+              <div>
+                {params.value.firstname} {params.value.lastname}
+              </div>
+            ),
+          },
+
+    ])
+        
+
+
+
+
+    return(
+        <div>
+            <div
+          style={{ height: 600, width: "90%" }}
+          className="ag-theme-material"
+        >
+          <AgGridReact
+            rowData={trainings}
+            columnDefs={columnDefs}
+            paginationPageSize={10}
+            pagination={true}
+          />
+        </div>
+        </div>
+    )
+}
